d9a5063d42525dc643815da80dbb51b04e670cec
German Solano <kontrolg1239@gmail.com>
2020-08-12 13:39:24 -0400

    Initial project folder

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { StrictMode } from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <StrictMode>
+      <h1>Hello world!</h1>
+    </StrictMode>
+  );
+};
+
+export default App;
