d1a3580ca4471540f09309f0f31cd38b31d2692f
German Solano <kontrolg1239@gmail.com>
2020-08-15 15:22:17 -0400

    Add ThemeProvider with blue theme

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,31 @@
 import React, { StrictMode } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import MainRouter from "./MainRouter";
+import {
+  ThemeProvider,
+  createMuiTheme,
+  responsiveFontSizes
+} from "@material-ui/core/styles";
+import blue from "@material-ui/core/colors/blue";
+
+const getApplicationTheme = () => {
+  const theme = createMuiTheme({
+    palette: {
+      primary: { main: blue[500] },
+      secondary: { main: blue[200] }
+    }
+  });
+  return responsiveFontSizes(theme);
+};
 
 const App = () => {
+  const applicationTheme = getApplicationTheme();
   return (
     <StrictMode>
       <CssBaseline />
-      <MainRouter />
+      <ThemeProvider theme={applicationTheme}>
+        <MainRouter />
+      </ThemeProvider>
     </StrictMode>
   );
 };
